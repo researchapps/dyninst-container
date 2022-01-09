fc02992bdce5ccd6c6468c457f19c35981d5d445
vsoch <vsoch@users.noreply.github.com>
2022-01-09 15:18:13 -0700

    adding ls
    Signed-off-by: vsoch <vsoch@users.noreply.github.com>

--- test.sh
+++ test.sh
@@ -27,8 +27,9 @@ printf "⭐️ Running tests...\n"
 cd /opt/dyninst-env/build/testsuite/tests
 export DYNINSTAPI_RT_LIB=/opt/dyninst-env/build/dyninst/lib/libdyninstAPI_RT.so
 export OMP_NUM_THREADS=2
-LD_LIBRARY_PATH=/opt/dyninst-env/build/dyninst/lib:$PWD:$LD_LIBRARY_PATH
-./runTests -64 -all -log test.log -j1 > >(tee stdout.log) 2> >(tee stderr.log >&2)
+export LD_LIBRARY_PATH=/opt/dyninst-env/build/dyninst/lib:$PWD:$LD_LIBRARY_PATH
+ls
+./runTests -64 -all -log test.log -j1 #> >(tee stdout.log) 2> >(tee stderr.log >&2)
 
 # 1>stdout.log 2>stderr.log                                     
 

e53bcfa954b3f416a8dc482dad344f8c2656aabf
vsoch <vsoch@users.noreply.github.com>
2022-01-09 20:57:34 -0700

    adding groups to organize output
    Signed-off-by: vsoch <vsoch@users.noreply.github.com>

--- test.sh
+++ test.sh
@@ -8,19 +8,23 @@ mkdir -p build/dyninst
 
 # 1. Build Dyninst
 printf "⭐️ Preparing to build Dyninst\n"
+echo "::group::build dyninst"   
 cd build/dyninst
 cmake -H/code -B. -DCMAKE_INSTALL_PREFIX=. > >(tee config.out) 2> >(tee config.err >&2)
 make VERBOSE=1 -j2 > >(tee build.out) 2> >(tee build.err >&2)
 make install VERBOSE=1 -j2 > >(tee build-install.out) 2> >(tee build-install.err >&2)
+echo "::endgroup::"
 
 # 2. Build the test suite
 printf "⭐️ Preparing to build the testsuite\n"
+echo "::group::build tests"   
 cd /opt/dyninst-env/
 mkdir -p build/testsuite/tests
 cd build/testsuite
 cmake -H/opt/testsuite -B. -DDyninst_DIR=/opt/dyninst-env/build/dyninst/lib/cmake/Dyninst > >(tee config.out) 2> >(tee config.err >&2)
 make VERBOSE=1 -j2 > >(tee build.out) 2> >(tee build.err >&2)
 make install VERBOSE=1 -j2 > >(tee build-install.out) 2> >(tee build-install.err >&2)
+echo "::endgroup::"
 
 # 3. Run the tests
 printf "⭐️ Running tests...\n"

8c7e2c38f205271fb29fd3919cbb1148d02a1f12
vsoch <vsoch@users.noreply.github.com>
2022-01-10 12:04:33 -0700

    add base container build of tests to streamline actual testing
    Signed-off-by: vsoch <vsoch@users.noreply.github.com>

---
+++ build.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+set -euo pipefail
+
+# This script builds dyninst and the test suite (but does not run tests)
+
+printf "⭐️ Setting up spack environment for Dyninst\n"
+. /opt/spack/share/spack/setup-env.sh
+spack env activate .
+mkdir -p build/dyninst
+
+# 1. Build Dyninst
+printf "⭐️ Preparing to build Dyninst\n"
+echo "::group::build dyninst"   
+cd build/dyninst
+cmake -H/code -B. -DCMAKE_INSTALL_PREFIX=. > >(tee config.out) 2> >(tee config.err >&2)
+make VERBOSE=1 -j2 > >(tee build.out) 2> >(tee build.err >&2)
+make install VERBOSE=1 -j2 > >(tee build-install.out) 2> >(tee build-install.err >&2)
+echo "::endgroup::"
+
+# 2. Build the test suite
+printf "⭐️ Preparing to build the testsuite\n"
+echo "::group::build tests"   
+cd /opt/dyninst-env/
+mkdir -p build/testsuite/tests
+cd build/testsuite
+
+cmake -H/opt/testsuite -B. -DCMAKE_INSTALL_PREFIX=$PWD/tests -DDyninst_DIR=/opt/dyninst-env/build/dyninst/lib/cmake/Dyninst > >(tee config.out) 2> >(tee config.err >&2)
+make VERBOSE=1 -j2 > >(tee build.out) 2> >(tee build.err >&2)
+make install VERBOSE=1 -j2 > >(tee build-install.out) 2> >(tee build-install.err >&2)
+echo "::endgroup::"
